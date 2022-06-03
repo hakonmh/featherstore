aac4814dc5cf648f8c977c4ea0b7e249631e9258
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-06-03 23:50:52 +0200

    Changed snapshots to no longer overwrite data by default

--- featherstore/snapshot.py
+++ featherstore/snapshot.py
@@ -3,12 +3,13 @@ import os
 import pickle
 from datetime import datetime
 from featherstore.connection import Connection, current_db
+from featherstore._table import _raise_if
 
 METADATA_FILE_NAME = 'metadata.pkl'
 
 
-def restore_table(store, source):
-    """Restores a table in to the currently selected db
+def restore_table(store, source, errors='raise'):
+    """Restores a table in to the currently selected db.
 
     Parameters
     ----------
@@ -16,21 +17,29 @@ def restore_table(store, source):
         The name of the store to restore the table into.
     source : str
         Path to the snapshot file.
+    errors : str
+        Whether or not to raise an error if a table with the same name already
+        exist. Can be either `raise` or `ignore`, `ignore` overwrites existing
+        table, by default `raise`.
     """
-    _can_restore_table(store, source)
+    _can_restore_table(store, source, errors)
     store_path = os.path.join(current_db(), store)
     _extract_snapshot(store_path, source)
 
 
-def restore_store(source):
+def restore_store(source, errors='raise'):
     """Restores a store in to the currently selected db.
 
     Parameters
     ----------
     source : str
         Path to the snapshot file.
+    errors : str
+        Whether or not to raise an error if a store with the same name already
+        exist. Can be either `raise` or `ignore`, `ignore` overwrites existing
+        store, by default `raise`.
     """
-    _can_restore_store(source)
+    _can_restore_store(source, errors)
     _extract_snapshot(current_db(), source)
 
 
@@ -44,20 +53,22 @@ def _extract_snapshot(output_path, source):
             tar.extract(member, output_path)
 
 
-def _can_restore_table(store, source):
+def _can_restore_table(store, source, errors):
     Connection._raise_if_not_connected()
     __raise_if_store_is_not_str(store)
     __raise_if_store_doesnt_exist(store)
     __raise_if_source_path_is_not_str(source)
     __raise_if_snapshot_not_found(source)
     __raise_if_not_snapshot_of_table(source)
+    __raise_if_table_already_exists(store, source, errors)
 
 
-def _can_restore_store(source):
+def _can_restore_store(source, errors):
     Connection._raise_if_not_connected()
     __raise_if_source_path_is_not_str(source)
     __raise_if_snapshot_not_found(source)
     __raise_if_not_snapshot_of_store(source)
+    __raise_if_store_already_exists(source, errors)
 
 
 def __raise_if_store_is_not_str(store):
@@ -94,6 +105,29 @@ def __raise_if_not_snapshot_of_table(source):
         raise ValueError("File is not a snapshot of a table")
 
 
+def __raise_if_table_already_exists(store, source, errors):
+    if errors == 'raise':
+        store_path = os.path.join(current_db(), store)
+
+        if '.tar.xz' not in source:
+            source = f'{source}.tar.xz'
+        with tarfile.open(source, "r") as tar:
+            table_name = tar.getnames()[0]
+
+        if table_name in os.listdir(store_path):
+            raise FileExistsError(f"A table with name {table_name} already exists")
+
+
+def __raise_if_store_already_exists(source, errors):
+    if '.tar.xz' not in source:
+        source = f'{source}.tar.xz'
+    with tarfile.open(source, "r") as tar:
+        store_name = tar.getnames()[0]
+
+    if store_name in os.listdir(current_db()) and errors == 'raise':
+        raise FileExistsError(f"A store with name {store_name} already exists")
+
+
 def __raise_if_not_snapshot_of_store(source):
     if '.tar.xz' not in source:
         source = f'{source}.tar.xz'
@@ -109,12 +143,36 @@ def __raise_if_not_snapshot_of_store(source):
 
 
 def _create_snapshot(output_path, input_path, input_type):
+    _can_create_snapshot(output_path, input_path, input_type)
     dir_path = os.path.split(output_path)[0]
     metadata_path = os.path.join(dir_path, METADATA_FILE_NAME)
 
     __create_snapshot_metadata(metadata_path, input_type)
     __write_snapshot(output_path, input_path, metadata_path)
     __delete_snapshot_metadata_file(metadata_path)
+
+
+def _can_create_snapshot(output_path, input_path, input_type):
+    Connection._raise_if_not_connected()
+    __raise_if_source_path_is_not_str(output_path)
+    __raise_if_input_doesnt_exists(input_path)
+    __raise_if_input_type_is_not_valid(input_type)
+
+
+def __raise_if_input_doesnt_exists(input_path):
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f"Snapshot target not found")
+
+
+def __raise_if_input_type_is_not_valid(input_type):
+    if input_type not in ('table', 'store'):
+        raise ValueError("Input type should be either 'table' or 'store")
+
+
+def _can_restore_store(source, errors):
+    __raise_if_snapshot_not_found(source)
+    __raise_if_not_snapshot_of_store(source)
+    __raise_if_store_already_exists(source, errors)
 
 
 def __create_snapshot_metadata(metadata_path, source_type):
