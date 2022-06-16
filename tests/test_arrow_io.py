3ca1f378da31439da804f26e34ce66a4e68379ff
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-06-17 01:24:54 +0200

    Refactored arrow io test

--- tests/test_arrow_io.py
+++ tests/test_arrow_io.py
@@ -9,30 +9,28 @@ from .fixtures import *
 @pytest.mark.parametrize("partition_size", [None, -1])
 def test_arrow_io(store, index, partition_size):
     original_df = make_table(index, astype='arrow')
-    expected = _sort_df(original_df)
-
     index_name = get_index_name(original_df)
+    expected = _sort_arrow_table(original_df, by=index_name)
+
     partition_size = _get_partition_size(original_df, partition_size)
-    store.write_table(TABLE_NAME,
-                      original_df,
-                      partition_size=partition_size,
-                      warnings="ignore",
-                      index=index_name)
+    table = store.select_table(TABLE_NAME)
     # Act
-    df = store.read_arrow(TABLE_NAME)
+    table.write(original_df, index=index_name, partition_size=partition_size,
+                warnings='ignore')
+    df = table.read_arrow()
     # Assert
     assert df.equals(expected)
+
+
+def _sort_arrow_table(df, *, by):
+    index_name = by
+    if index_name:
+        sorted_index = pa.compute.sort_indices(df[index_name])
+        df = df.take(sorted_index)
+    return df
 
 
 def _get_partition_size(df, partition_size):
     if partition_size is None:
         partition_size = get_partition_size(df)
     return partition_size
-
-
-def _sort_df(df):
-    index_name = get_index_name(df)
-    if index_name:
-        sorted_index = pa.compute.sort_indices(df[index_name])
-        df = df.take(sorted_index)
-    return df
