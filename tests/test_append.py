4d2239388ab75c26fa423614f46770e8971da339
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-06-17 16:32:43 +0200

    Refactored append tests

--- tests/test_append.py
+++ tests/test_append.py
@@ -9,37 +9,45 @@ from .fixtures import *
                          ["arrow", "polars", "pandas"])
 def test_append_table(store, index, astype):
     # Arrange
-    fixture = AppendFixtures(index=index, astype=astype)
+    fixture = AppendFixture(index=index, astype=astype)
     original_df = fixture.original_df
-    appended_df = fixture.appended_df
+    append_df = fixture.appended_df
+    expected = _append(append_df, to=original_df)
 
     partition_size = get_partition_size(original_df)
     index_name = get_index_name(original_df)
     table = store.select_table(TABLE_NAME)
     table.write(original_df, partition_size=partition_size, index=index_name)
     # Act
-    table.append(appended_df)
+    table.append(append_df)
     # Assert
-    fixture.assert_equal_expected(table)
+    _assert_table_equals(table, expected)
 
 
-class AppendFixtures:
+class AppendFixture:
 
-    def __init__(self, index=None, rows=30, cols=5, slice_=0.5, astype="pandas"):
+    def __init__(self, index, rows=30, astype="pandas"):
+        self._SLICE = round(rows * 0.5)
         self._astype = astype
-        self._df = make_table(index=index, rows=rows, cols=cols, astype=self._astype)
-        original_slice = round(rows * slice_)
-        self.original_df = self._df[:original_slice]
-        self._make_appended_df(original_slice, index)
+        self._index = index
+        self._df = make_table(self._index, rows=rows, astype=self._astype)
 
-    def _make_appended_df(self, original_slice, index):
-        df = self._df[original_slice:]
-        df = convert_table(df, to='pandas', as_series=False)
+    @property
+    def original_df(self):
+        df = self._df[:self._SLICE]
+        return df
+
+    @property
+    def appended_df(self):
+        df = convert_table(self._df, to='pandas', as_series=False)
+
+        df = df[self._SLICE:]
         df = self.__shuffle_cols(df)
-        if index is None:
+        if self._index is default_index:
             df = self.__make_default_index(df)
+
         df = convert_table(df, to=self._astype)
-        self.appended_df = df
+        return df
 
     def __shuffle_cols(self, df):
         cols = df.columns
@@ -52,29 +60,49 @@ class AppendFixtures:
         df = df.reset_index(drop=True)
         return df
 
-    def assert_equal_expected(self, table):
-        expected = self._make_expected()
 
-        if self._astype == "arrow":
-            df = table.read_arrow()
-            if set(df.column_names) == set(expected.column_names):
-                expected = expected.select(df.column_names)
-            assert df.equals(expected)
+def _append(df, *, to):
+    astype = __get_astype(to)
+    index_name = get_index_name(to)
+    original_df = convert_table(to, to="pandas", index_name=index_name)
+    append_df = convert_table(df, to="pandas", index_name=index_name)
+    df = pd.concat([original_df, append_df])
+    df = convert_table(df, to=astype, index_name=index_name)
+    return df
 
-        elif self._astype == 'polars':
-            df = table.read_polars()
-            if set(df.columns) == set(expected.columns):
-                expected = expected[df.columns]
-            assert df.frame_equal(expected)
 
-        else:
-            df = table.read_pandas()
-            assert all(df.eq(expected))
+def __get_astype(df):
+    if isinstance(df, (pd.DataFrame, pd.Series)):
+        return "pandas"
+    if isinstance(df, pl.DataFrame):
+        return "polars"
+    if isinstance(df, pa.Table):
+        return "arrow"
 
-    def _make_expected(self):
-        index_name = get_index_name(self._df)
-        expected = convert_table(self._df, to=self._astype, index_name=index_name)
-        return expected
+
+def _assert_table_equals(table, expected):
+    astype = __get_astype(expected)
+    if astype == "arrow":
+        df = table.read_arrow()
+        expected = __reorganize_cols(df, expected)
+        assert df.equals(expected)
+    elif astype == 'polars':
+        df = table.read_polars()
+        expected = __reorganize_cols(df, expected)
+        assert df.frame_equal(expected)
+    else:
+        df = table.read_pandas()
+        assert all(df.eq(expected))
+
+
+def __reorganize_cols(df, expected):
+    if isinstance(df, pa.Table):
+        if set(df.column_names) == set(expected.column_names):
+            expected = expected.select(df.column_names)
+    if isinstance(df, pl.DataFrame):
+        if set(df.columns) == set(expected.columns):
+            expected = expected[df.columns]
+    return expected
 
 
 def _wrong_index_dtype():
