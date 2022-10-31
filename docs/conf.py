2dc3b4a1f3fec3cb5aeb012635f3ecceca1a7523
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-10-31 16:45:21 +0100

    Changed version to 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Håkon Magne Holmen'
 author = 'Håkon Magne Holmen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 
