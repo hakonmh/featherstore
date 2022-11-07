afb48b850dc9069f689573e4aedf59c3d21454ff
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-11-08 00:50:02 +0100

    Added light logo for dark mode

--- docs/conf.py
+++ docs/conf.py
@@ -64,9 +64,12 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/logo.png"
 html_favicon = '_static/favicon.ico'
 html_theme_options = {
+    "logo": {
+        "image_light": "_static/logo-dark.png",
+        "image_dark": "_static/logo-light.png",
+    },
     "icon_links": [
         {
             "name": "GitHub",
