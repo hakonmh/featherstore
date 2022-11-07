972bf91632843e3ca3184b0e40451f2eb4f4237f
Håkon Magne Holmen <haakonholmen@hotmail.com>
2022-11-08 00:37:38 +0100

    Added source code links in api reference

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@
 #
 import os
 import sys
+import inspect
+import warnings
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
@@ -32,6 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
+    "sphinx.ext.linkcode",
 ]
 
 autosummary_generate = True
@@ -52,10 +55,10 @@ exclude_patterns = ['_build', '_templates', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
+html_show_sourcelink = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'pydata_sphinx_theme'
-# html_theme_path = ["/home/hakonmh/.local/lib/python3.9/site-packages/"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -63,3 +66,65 @@ html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_logo = "_static/logo.png"
 html_favicon = '_static/favicon.ico'
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/hakonmh/featherstore",
+            "icon": "fa-brands fa-github"
+        }
+    ]
+}
+
+
+def linkcode_resolve(domain, info):
+    """
+    Determine the URL corresponding to Python object.
+    Based on pandas equivalent:
+    https://github.com/pandas-dev/pandas/blob/main/doc/source/conf.py#L629-L686
+    """
+    if domain != 'py':
+        return None
+    modname = info["module"]
+    filename = modname.replace('.', '/')
+    fullname = info["fullname"]
+
+    submod = sys.modules.get(modname)
+    if submod is None:
+        return None
+
+    obj = submod
+    for part in fullname.split("."):
+        try:
+            with warnings.catch_warnings():
+                # Accessing deprecated objects will generate noisy warnings
+                warnings.simplefilter("ignore", FutureWarning)
+                obj = getattr(obj, part)
+        except AttributeError:
+            return None
+
+    try:
+        fn = inspect.getsourcefile(inspect.unwrap(obj))
+    except TypeError:
+        try:  # property
+            fn = inspect.getsourcefile(inspect.unwrap(obj.fget))
+        except (AttributeError, TypeError):
+            fn = None
+    if not fn:
+        return None
+
+    try:
+        source, lineno = inspect.getsourcelines(obj)
+    except TypeError:
+        try:  # property
+            source, lineno = inspect.getsourcelines(obj.fget)
+        except (AttributeError, TypeError):
+            lineno = None
+    except OSError:
+        lineno = None
+
+    if lineno:
+        lineno = f"#L{lineno}-L{lineno + len(source) - 1}"
+    else:
+        lineno = ""
+    return f"https://www.github.com/hakonmh/featherstore/tree/master/{filename}.py{lineno}"
